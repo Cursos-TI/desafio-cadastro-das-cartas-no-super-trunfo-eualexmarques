5e39c320662270b53107f3fdd944c40896fa90ed
eualexmarques <frejits@gmail.com>
2025-03-29 19:17:11 +0000

    supertrufo finalizado

---
+++ SuperTunfo.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+
+int main(){
+
+    int pontos_turisticos;
+    int Pontos_turisticos;
+    int habitantes;
+    int Habitantes;
+    float area;
+    float area02;
+    float PIB;
+    float PIB02;
+    char estado [8] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'};
+    char Estado [8] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'};
+    char nome_cidade [20];
+    char Nome_cidade [20];
+    char codigo [4];
+    char Codigo [4];
+
+    printf ("carta 01\n");
+    printf ("Digite o nome da cidade:\n");
+    scanf ("%s", nome_cidade);
+    printf ("Digite o codigo:\n");
+    scanf ("%s", &codigo);
+    printf ("Digite o estado:\n");
+    scanf ("%s", &estado);
+    printf ("Digite quantos habitantes:\n");
+    scanf ("%d", &habitantes);
+    printf ("Digite quantos pontos turisticos:\n");
+    scanf ("%d", &pontos_turisticos);
+    printf ("Digite qual a áre:\n");
+    scanf ("%f", &area);
+    printf ("Digite qual o PIB:\n");
+    scanf ("%f", &PIB);
+
+    printf ("Carta 02\n");
+    printf ("Digite o nome da cidade:\n");
+    scanf ("%s", Nome_cidade);
+    printf ("Digite o codigo:\n");
+    scanf ("%s", &Codigo);
+    printf ("Digite o estado:\n");
+    scanf ("%s", &Estado);
+    printf ("Digite quantos habitantes:\n");
+    scanf ("%d", &Habitantes);
+    printf ("Digite quantos pontos turisticos:\n");
+    scanf ("%d", &Pontos_turisticos);
+    printf ("Digite qual a áre:\n");
+    scanf ("%f", &area02);
+    printf ("Digite qual o PIB:\n");
+    scanf ("%f", &PIB02);
+    
+    //Agora pegamos os dados anotados e montamos as cartas
+
+    ;printf ("Carta 01\n");
+    
+    printf ("Estado: %s\n", estado);
+    printf ("Codigo: %s\n", codigo);
+    printf ("Cidade: %s\n", nome_cidade);
+    printf ("População: %d\n", habitantes);
+    printf ("Área: %.2f\n", area);
+    printf ("PIB: %.2f\n", PIB);
+    printf ("Pontos turisticos: %d\n", pontos_turisticos);
+
+    printf ("Carta 02\n");
+    printf ("Cidade: %s\n", Nome_cidade);
+    printf ("Codigo: %s\n", Codigo);
+    printf ("Estado: %s\n", Estado);
+    printf ("População: %d\n", Habitantes);
+    printf ("Pontos turisticos: %d\n", Pontos_turisticos);
+    printf ("Area: %.2f\n", area02);
+    printf ("PIB: %.2f\n", PIB02);
+
+
+    return 0;
+
+}
